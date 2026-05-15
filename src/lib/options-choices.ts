96d1e94b43ffe870e806f1e4f4801fa225ba4ab0
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-14 23:17:17 -0300

    feat: inital inquier config object

---
+++ src/lib/options-choices.ts
@@ -0,0 +1,20 @@
+/**
+ * Inquirer/Prompt Config options
+ */
+const options = {
+  pkg_manager: {
+    message: "",
+    choices: [],
+  },
+  pkg: {
+    message: "",
+  },
+  deps: {
+    message: "",
+  },
+  finish: {
+    message: "",
+  },
+};
+
+export default options;
