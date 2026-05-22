166828b11981ec43555919d2867c3acbceee9c2c
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-21 23:38:36 -0300

    feat: turbo repo command listing

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Object --> Turborepo ls command --> list WS folders
+ * See docs: https://turborepo.dev/docs/reference/ls
+ */
+export const TR_CMD = {
+  ls: "turbo ls",
+  flag: "--output=json",
+  jq: `| jq -r ".packages.items"`,
+};
