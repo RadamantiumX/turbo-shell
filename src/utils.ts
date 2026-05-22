9cf4fd1d31ce27596c4b66df2b0bda98832e32b6
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-21 23:38:10 -0300

    feat: parsed data fn

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,13 @@
-export function parsedData(data) {
-  const takenData = [];
-  JSON.parse(data).map((item) => {
-    takenData.push({});
+export function parsedStreamData(data: string) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const parsedData: any = [];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  JSON.parse(data).map((item: any) => {
+    parsedData.push({
+      name: item.name,
+      value: item.name,
+      description: item.path,
+    });
   });
-
-  return takenData;
+  return parsedData;
 }
