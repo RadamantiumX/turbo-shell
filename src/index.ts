4a4c37dc6c044a207ec8316576806afd2d503e15
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-24 23:50:33 -0300

    feat: starting with main fn

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export async function wsMainInterface() {}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getWorkSpaceFolders } from "./streams/sub-process";
+import { colorConsole } from "./console/colors";
+
+const turbo = getWorkSpaceFolders();
+
+turbo?.on("data", wsMainInterface);
+
+export async function wsMainInterface(data: any) {
+  try {
+    console.log(data);
+  } catch (error) {
+    colorConsole(`${error}`, "error");
+  }
+}
