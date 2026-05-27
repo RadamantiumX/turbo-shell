88b3dc126eae9416e22cafd26aa8579261b6d435
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-26 23:18:25 -0300

    feat: usign cross-env

--- src/common/parsed.ts
+++ src/common/parsed.ts
@@ -4,9 +4,9 @@ export function parsedStreamData(data: string) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   JSON.parse(data).map((item: any) => {
     options.ws_folders.choices.push({
-      name: item.name,
-      value: item.name,
-      description: item.path,
+      name: item?.name,
+      value: item?.name,
+      description: item?.path,
     });
   });
   return;
