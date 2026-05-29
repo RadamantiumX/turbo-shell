2cebe42a950e857f90996edd4b4bbd16bc9843e2
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-29 18:03:09 -0300

    feat: adding mocking values and entries arguments

--- src/test/mock/mocking-values.ts
+++ src/test/mock/mocking-values.ts
@@ -29,3 +29,42 @@ export const mockedOptions = {
 
 export const mockStringData =
   '[{"name": "firstName", "path": "//path_1"}, {"name": "secondName", "path": "//path_2"}, {"name": "thirdName", "path": "//path_3"}]';
+
+export const mockingCommandArgs = [
+  {
+    selectFolder: "web",
+    selectPkgManager: "npm",
+    inputPkg: "react",
+    depsConfirm: true,
+  },
+  {
+    selectFolder: "package",
+    selectPkgManager: "pnpm",
+    inputPkg: "tsup",
+    depsConfirm: true,
+  },
+  {
+    selectFolder: "api",
+    selectPkgManager: "yarn",
+    inputPkg: "inquirer",
+    depsConfirm: true,
+  },
+];
+
+export const mockCommandReturn = [
+  {
+    first: "npm i",
+    second: "react",
+    third: "--workspace=web -D",
+  },
+  {
+    first: "pnpm add",
+    second: "",
+    third: "",
+  },
+  {
+    first: "npm i",
+    second: "",
+    third: "",
+  },
+];
