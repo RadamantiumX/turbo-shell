056a149bcf55bcdf67b8a5b6d7ae71cf0a89001d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-05-29 18:02:36 -0300

    feat: adding some comments

--- src/common/parsed.ts
+++ src/common/parsed.ts
@@ -1,5 +1,10 @@
 import options from "../lib/options-choices";
 
+/**
+ * Receive the raw data and parsed to push on inquirer options choices.
+ * @param data String raw data comming from child process
+ * @returns
+ */
 export function parsedStreamData(data: string) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   JSON.parse(data).map((item: any) => {
